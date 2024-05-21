fe7d24e54235bdbe1beef0fe435327734c65010b
Puljetic, Miljan <m.puljetic@extern.deutsche-glasfaser.de>
2024-05-22 00:14:56 +0200

    local

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,12 +1,18 @@
 package com.example.demo.entity;
 
 
+//import com.redis.om.spring.annotations.Indexed;
+//import com.redis.om.spring.annotations.SchemaFieldType;
+//import com.redis.om.spring.annotations.SerializationHint;
+import com.redis.om.spring.annotations.IndexCreationMode;
 import com.redis.om.spring.annotations.Indexed;
-import com.redis.om.spring.annotations.SchemaFieldType;
-import com.redis.om.spring.annotations.SerializationHint;
+import com.redis.om.spring.annotations.IndexingOptions;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.redis.core.RedisHash;
+//import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +20,7 @@ import java.time.LocalDateTime;
 @Builder
 @RequiredArgsConstructor(staticName = "of")
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@IndexingOptions(creationMode = IndexCreationMode.SKIP_ALWAYS, indexName = "radius:acct:index:acct-idx")
 @RedisHash
 public class Student {
 
@@ -23,8 +30,11 @@ public class Student {
     @Indexed(alias = "User-Name")
     private String userName;
 
-    @Indexed(alias = "Event-Timestamp")
-    private LocalDateTime eventTimestamp;
+    @Indexed(alias = "Acct-Session-Id")
+    private String acctSessionId;
+
+   // @Indexed
+    //private LocalDateTime eventTimestamp;
 
 
 }
